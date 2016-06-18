fba10c850c832b90cbbc3ea7a42a3193e1699bf7
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2016-06-18 15:01:05 +0200

    Howto tiles

--- purecss-config.js
+++ purecss-config.js
@@ -3,8 +3,8 @@ const PURECSS_PATH = './node_modules/purecss/build/';
 export default [
   'base',
   'grids-core',
-  'grids-responsive',
   'grids-units',
+  'grids-responsive',
   'menus-core',
   'menus-horizontal',
 ].map(module => `${PURECSS_PATH}${module}.css`);
