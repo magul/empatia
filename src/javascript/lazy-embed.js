06297543f008915a74ed553d9d9ebea1068b4d3e
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2016-06-26 19:37:58 +0200

    Prevent zooming map while scrolling

--- src/javascript/lazy-embed.js
+++ src/javascript/lazy-embed.js
@@ -1,9 +1,12 @@
 const LOADING_CLASS = 'is-loading';
+const IGNORE_MOUSE_CLASS = 'is-blurred';
 
 export function lazyEmbed({ element }) {
   element
+    .toggleClass(IGNORE_MOUSE_CLASS, true)
+    .on('click', () => { element.toggleClass(IGNORE_MOUSE_CLASS, false); })
+    .on('mouseleave', () => { element.toggleClass(IGNORE_MOUSE_CLASS, true); })
+    .children()
     .attr('src', element.data().src)
-    .on('load', () => {
-      element.removeClass(LOADING_CLASS);
-    });
+    .on('load', () => { element.removeClass(LOADING_CLASS); });
 }
