42c5b7e3fa12e4822d20131e2ad78c6f78f637f7
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2016-06-28 22:36:43 +0200

    If iframe load event wasn't called show it anyway

--- src/javascript/lazy-embed.js
+++ src/javascript/lazy-embed.js
@@ -1,8 +1,10 @@
 const LOADING_CLASS = 'is-loading';
 const IGNORE_MOUSE_CLASS = 'is-blurred';
+const LOADING_TIMEOUT = 5000;
 
 export function lazyEmbed({ element }) {
   const iframeElement = element.querySelector('iframe');
+  let timeoutFallback;
 
   function handleClick() {
     element.classList.remove(IGNORE_MOUSE_CLASS);
@@ -14,6 +16,7 @@ export function lazyEmbed({ element }) {
 
   function handleIframeLoad() {
     element.classList.remove(LOADING_CLASS);
+    clearTimeout(timeoutFallback);
   }
 
   element.classList.add(IGNORE_MOUSE_CLASS);
@@ -21,4 +24,5 @@ export function lazyEmbed({ element }) {
   element.addEventListener('mouseleave', handleMouseLeave, false);
   iframeElement.addEventListener('load', handleIframeLoad, false);
   iframeElement.setAttribute('src', element.dataset.src);
+  timeoutFallback = setTimeout(handleIframeLoad, LOADING_TIMEOUT);
 }
