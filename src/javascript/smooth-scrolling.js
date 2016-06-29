7ce461c2e33d84b3a48277d9e60df79b86224547
Piotr Wojtkowiak <pwojtkowiak@gmail.com>
2016-06-29 21:23:21 +0200

    Cancel scroll animation on mousewheel

--- src/javascript/smooth-scrolling.js
+++ src/javascript/smooth-scrolling.js
@@ -4,17 +4,26 @@ import { NAVIGATION_TOPIC, NAVIGATION_CHANGE, smoothScroll } from './events';
 
 const ANIMATION_DURATION = 500;
 const ANIMATION_EASE = 'inOutQuart';
+const CANCEL_EVENT = 'wheel';
 
 export function smoothScrolling() {
+  const animationOptions = {
+    diration: ANIMATION_DURATION,
+    ease: ANIMATION_EASE,
+  };
+
   PubSub.subscribe(NAVIGATION_TOPIC, (topic, { type, target }) => {
     if (type === NAVIGATION_CHANGE) {
       const scrollTarget = document.querySelector(target);
+      const animation = scrollTo(0, scrollTarget.offsetTop, animationOptions);
+      const cancelCallback = () => { animation.stop(); };
+      const onAnimationEnd = () => {
+        document.removeEventListener(CANCEL_EVENT, cancelCallback);
+      };
 
+      animation.once('end', onAnimationEnd);
+      document.addEventListener(CANCEL_EVENT, cancelCallback, false);
       smoothScroll(true);
-      scrollTo(0, scrollTarget.offsetTop, {
-        diration: ANIMATION_DURATION,
-        ease: ANIMATION_EASE,
-      });
       setTimeout(smoothScroll, ANIMATION_DURATION, false);
     }
   });
